e37d26a7b09f4bf11a3d681f6a07f66d2283052e
Alexandre Dourado <oalexandredourado@gmail.com>
2026-03-21 19:58:32 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mindvault/',  // adicione esta linha
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
